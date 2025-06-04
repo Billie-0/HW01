5913b2b4e3239c0ab50ee49b5960b697f63abbf1
Billie-0 <binegerton@gmail.com>
2025-06-04 17:02:06 +0900

    solve HW01

---
+++ HW01/HW01.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int getSum(const vector<int>& arr);
+double getAverage(const vector<int>& arr);
+
+int main() {
+	int size;
+	cout << "몇 개의 정수를 입력할 건가요? : ";
+	cin >> size;
+
+	vector<int> numbers(size);
+
+	cout << size << "개의 정수를 입력하세요 : ";
+	for (int i = 0; i < size; i++) {
+		cin >> numbers[i];
+	}
+
+	int sum = getSum(numbers);
+	double avg = getAverage(numbers);
+
+	cout << "합계 : " << sum << endl;
+	cout << "평균 : " << avg << endl;
+
+	return 0;
+}
+
+int getSum(const vector<int>& arr) {
+	int sum = 0;
+	for (int i = 0; i < arr.size(); i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
+
+double getAverage(const vector<int>& arr) {
+	int sum = getSum(arr);
+	return (double)sum / arr.size();
+}
